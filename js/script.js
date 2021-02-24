4c6a8d4451510f40734a11a33b0e341d2259d4b6
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-02-24 16:21:58 +0300

    persisting  error

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
-
-const akanForm = document.getElementById(akanForm);//targeting the id akanForm
-const maleNames =["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Kwaku", "Yaw", "Kofi", "Kwame"];
-const femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-akanForm.addEventListener("submit",(e)=> {
-    e.preventDefault();//prevents the browser from  running the page by  default.
-    const formData = new formData(e.target) // this becomes the new form that has been filled arcordingly.
-    var dateEntered = new Date(formData.get("date"));// this will convert the  dates entered into objects
-    if (formData.get("gender")== "male") {
-        document.getElementById("message").innerHTML="Akan name is" + maleNames[dateEntered.getDay()];
-     } else formData.get("gender")== "female") {
-            document.getElementById("message").innerHTML="Akan name is" + femaleNames[dateEntered.getDay()];
-     }
- 
-    });
-
+$(document).ready(function() {
+    const akanForm = document.getElementById("akanForm");//targeting the id akanForm
+    const maleNames =["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    const femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    akanForm.addEventListener("submit",(e)=> {
+      e.preventDefault();//prevents the browser from  running the page by  default.
+      const formData = new FormData(e.target); // this becomes the new form that has been filled arcordingly.
+      var dateEntered = new Date(formData.get("date"));// this will convert the  dates entered into objects
+      if (formData.get("gender")== "male") {
+          document.getElementById("message").innerHTML="Akan name is" + maleNames[dateEntered.getDay()];
+      } else {
+          document.getElementById("message").innerHTML="Akan name is" + femaleNames[dateEntered.getDay()];
+       }
+  });
+});
